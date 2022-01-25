591352efc3c5ca817d87fb8735a4009787efddc1
강현준 <hyunjune94@naver.com>
2022-01-25 17:40:47 +0900

    commit

--- mysql/exc/exc.sql
+++ mysql/exc/exc.sql
@@ -62,3 +62,35 @@ select * from cafe where name like '%가커%';
 select * from cafe where name like '마%';
 select * from cafe where name like '%A';
  use world;
+ 
+-- 테이블 수정
+-- 컬럼 추가
+alter table test add tel varchar(50);
+desc test; -- 테이블 상태 확인(dexcribe)
+
+-- 컬럼 삭제
+alter table test drop tel;
+
+-- 컬럼 타입 변경
+alter table test modify tel int;
+alter table test modify tel varchar(45);
+
+-- 컬럼 이름 변경
+alter table test change tele tel int;
+
+select * from test;
+
+-- 데이터 수정
+update test set
+	name = 'Andrew'
+    ,tel = '222'
+where 1=1
+	and seq = 1;
+
+-- 데이터 삭제
+delete from test
+where 1=1
+	and seq = 2;
+
+-- 전체 데이터 삭제
+truncate test;
